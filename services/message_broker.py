c668c000c0f3745dd3586d5405fb778503e5cb01
IasonTucK <iasonkourmoulis@gmail.com>
2026-04-25 14:34:40 +0300

    Remove .env from tracking

--- services/message_broker.py
+++
@@ -1,8 +0,0 @@
-class MessageBroker:
-
-    async def broadcast_config_update(self, max_workers: int) -> None:
-        """
-        Publishes a 'ConfigUpdated' event to a RabbitMQ Fanout Exchange.
-        This broadcasts the new worker limits to all active Manager replicas simultaneously.
-        """
-        pass
